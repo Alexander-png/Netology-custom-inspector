3f25e350794b34b92b25204aa0f5fd079b8742a1
Alexander <sanek41km@mail.ru>
2022-09-13 23:06:22 +0300

    Strange commit

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Lesson_6_8
+{
+    public class NewBehaviourScript : MonoBehaviour
+    {
+        [SerializeField, Range(0,5)]
+        private float someFloat;
+    }
+}
